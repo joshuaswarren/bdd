8423af59c6cb21e8d65a13fe3481818f8681ead6
Joshua Warren <josh@creatuity.com>
2015-05-18 22:20:21 -0500

    Describing our Timeoff class in a spec

--- spec/TimeoffSpec.php
+++ spec/TimeoffSpec.php
@@ -11,4 +11,25 @@ class TimeoffSpec extends ObjectBehavior
     {
         $this->shouldHaveType('App\Timeoff');
     }
+
+    function it_creates_timeoff_requests() {
+        $this->create("Name", "reason")->shouldReturn(true);
+    }
+    
+    function it_loads_all_timeoff_requests() {
+        $this->loadAll()->shouldReturnArray();
+    }
+
+    function it_loads_pending_timeoff_requests() {
+        $this->loadPending()->shouldReturnArray();
+    }
+
+    function it_approves_timeoff_requests() {
+        $this->approve("id")->shouldReturn(true);
+    }
+
+    function it_denies_timeoff_requests() {
+        $this->deny("id")->shouldReturn(true);
+    }
+
 }
